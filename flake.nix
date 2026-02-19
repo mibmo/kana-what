304ef611d03e9b0993af2be3ef1eee9229f488b0
mib <mib@kanp.ai>
2026-02-19 21:39:56 +0100

    nix packages & set up styling

--- flake.nix
+++ flake.nix
@@ -20,16 +20,159 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
     inputs@{ self, conch, ... }:
     conch.load (
-      { pkgs, ... }:
+      {
+        config,
+        lib,
+        pkgs,
+        system,
+        ...
+      }:
       let
+        inherit (lib.fileset)
+          fileFilter
+          intersection
+          toSource
+          unions
+          ;
+
+        project-name = "kana-what";
+        version = self.shortRev or "dirty";
+
         nodejs = pkgs.nodejs_25;
-        pnpm = pkgs.pnpm_9;
+        pnpm = pkgs.pnpm_10;
         wasm-bindgen-cli = pkgs.wasm-bindgen-cli_0_2_100;
+
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+          p:
+          p.rust-bin.nightly.latest.default.override {
+            inherit (config.rust) targets;
+          }
+        );
+
+        commonArgs = {
+          inherit version;
+          strictDeps = true;
+          cargoExtraArgs = "--target wasm32-unknown-unknown";
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly (
+          commonArgs
+          // {
+            pname = project-name;
+
+            src = toSource {
+              root = ./.;
+              fileset = unions [
+                ./Cargo.lock
+                ./Cargo.toml
+              ];
+            };
+          }
+        );
+
+        frontend = craneLib.buildPackage (
+          commonArgs
+          // {
+            pname = "${project-name}-frontend";
+            inherit cargoArtifacts;
+
+            doCheck = false;
+
+            src = toSource {
+              root = ./.;
+              fileset = unions [
+                (fileFilter (file: file.hasExt "rs") ./.)
+                ./Cargo.lock
+                ./Cargo.toml
+                ./index.html
+              ];
+            };
+
+            nativeBuildInputs = [
+              pkgs.trunk
+              wasm-bindgen-cli
+            ];
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p style
+              cp "${stylesheet}" style/main.css
+
+              trunk build --release --dist "$out"
+
+              runHook postInstall
+            '';
+          }
+        );
+
+        stylesheet = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "${project-name}-stylesheet";
+          inherit version;
+
+          src = toSource {
+            root = ./.;
+            fileset = unions [
+              ./package.json
+              ./pnpm-lock.yaml
+              ./postcss.config.js
+              ./style
+            ];
+          };
+
+          nativeBuildInputs = [
+            nodejs
+            pkgs.pnpmConfigHook
+            pnpm
+          ];
+
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit (finalAttrs) pname version src;
+            fetcherVersion = 3;
+            hash = "sha256-2F0UIQdrjuopkjS7nbpGS5MvJsc3Rc/5LSUaWCyY+AA=";
+          };
+
+          installPhase = ''
+            runHook preInstall
+
+            pnpm exec postcss style/main.css > $out
+
+            runHook postInstall
+          '';
+        });
+
+        site-artifacts = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "${project-name}-site-artifacts";
+          inherit version;
+
+          src = ./public;
+
+          installPhase = ''
+            runHook preInstall
+
+            # symlink stylesheet and frontend files
+            mkdir -p "$out"
+            find "${frontend}" -type f | while read -r file; do
+              ln -s "$file" "$out/$(basename "$file")"
+            done
+
+            # create directory structure
+            find . -type d -exec mkdir -p "$out/{}" \;
+            # symlink public artifacts
+            find . -type f | sed 's|^\./||' | while read -r file; do
+              ln -s "$src/$file" "$out/$file"
+            done
+
+            runHook postInstall
+          '';
+        });
       in
       {
         imports = [
@@ -47,7 +190,6 @@
           environment.RUSTFLAGS = ''--cfg getrandom_backend="wasm_js"'';
           packages = with pkgs; [
             bacon
-            binaryen
             cargo-sort
             leptosfmt
             nodejs
@@ -56,6 +198,14 @@
             trunk
             wasm-bindgen-cli
           ];
+        };
+
+        flake.packages.${system} = {
+          inherit
+            stylesheet
+            frontend
+            site-artifacts
+            ;
         };
       }
     );
